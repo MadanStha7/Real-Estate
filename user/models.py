a6974700f20efb0df9d55d61a5e3a7be4ab2f47e
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-12-26 18:37:47 +0545

    create user profile and agent detail model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+IDENTIFICATION_TYPE = (
+    ('citizenship', 'Citizenship'),
+    ('driving-license', 'Driving License'),
+    ('others', 'Others')
+)
+
+
+class UserProfile(models.Model):
+    """
+    user profile
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    profile_picture = models.ImageField(upload_to='user/profile')
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}"
+
+    class Meta:
+        ordering = ['-added_at']
+        db_table = 'user_profile'
+
+
+class AgentDetail(models.Model):
+    """
+    agent detail
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='agent_detail')
+    location = models.TextField()
+    identification_type = models.CharField(choices=IDENTIFICATION_TYPE, max_length=20)
+    identification_number = models.CharField(max_length=20)
+    identification_file = models.FileField(upload_to='agent/identification')
+    accept_terms_and_condition = models.BooleanField(default=False)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}"
+
+    class Meta:
+        ordering = ['-added_at']
+        db_table = 'agent_detail'
