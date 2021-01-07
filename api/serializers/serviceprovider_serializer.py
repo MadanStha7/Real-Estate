e3e6be17c1847991e4c6020602dd6cfab1ccde71
Reebika Bhatta <66770073+iamreebika@users.noreply.github.com>
2021-01-07 16:42:37 +0545

    Files Added

--- api/serializers/serviceprovider_serializer.py
+++ api/serializers/serviceprovider_serializer.py
@@ -4,16 +4,15 @@ from service_provider.models import ServiceProvider
 
 class ServiceProviderSerializer(serializers.ModelSerializer):
 
-    user_name = serializers.CharField(source='user.username')
-    media = serializers.FileField(source="user.service")
+    username = serializers.CharField(source='user.username')
     custom_field = serializers.SerializerMethodField()
 
     class Meta:
         model = ServiceProvider
-        fields = ("user_name", "service_name", "company_name",
+        fields = ("user", "username", "service_name", "company_name",
                   "location", "price", "contact_number",
                   "description", "photos_or_videos",
                   "added_at", "custom_field")
 
     def get_custom_field(self, obj):
-        return dict(a=1, b=3, uid=obj.uid)
+        return dict(a=1, b=3)
