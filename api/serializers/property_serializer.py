610a342469b4dec72df8759d9e5a3239f3296926
ramthapa <thaparam78666@gmail.com>
2021-04-08 13:54:54 +0545

    contact model and api updated

--- api/serializers/property_serializer.py
+++ api/serializers/property_serializer.py
@@ -16,6 +16,7 @@ class PropertySerializer(serializers.ModelSerializer):
     """
     return the property details
     """
+
     class Meta:
         model = PropertyInfo
         fields = (
@@ -65,6 +66,25 @@ class GallerySerializer(serializers.ModelSerializer):
 
 
 class AmenitiesSerializer(serializers.ModelSerializer):
+    balcony_value = serializers.CharField(
+        source="get_balcony_display", read_only=True
+    )
+    water_supply_value = serializers.CharField(
+        source="get_water_supply_display", read_only=True
+    )
+    
+    gym_value = serializers.CharField(
+        source="get_gym_display", read_only=True
+    )
+    non_veg_value = serializers.CharField(
+        source="get_non_veg_display", read_only=True
+    )
+    security_value = serializers.CharField(
+        source="get_security_display", read_only=True
+    )
+    viewer_value = serializers.CharField(
+        source="get_viewer_display", read_only=True
+    )
     class Meta:
         model = Amenities
         fields = (
@@ -78,6 +98,13 @@ class AmenitiesSerializer(serializers.ModelSerializer):
             "viewer",
             "secondary_number",
             "property_info",
+            'balcony_value',
+            "water_supply_value",
+            "gym_value",
+            "non_veg_value",
+            "security_value",
+            "viewer_value",
+
         )
 
 
@@ -85,19 +112,41 @@ class RentalSerializer(serializers.ModelSerializer):
     """
     Rental
     """
+    available_for_value = serializers.CharField(
+        source="get_available_for_display", read_only=True
+    )
+    maintenance_value = serializers.CharField(
+        source="get_maintenance_display", read_only=True
+    )
+
+    tenants_value = serializers.CharField(
+        source="get_tenants_display", read_only=True
+    )
+    furnishing_value = serializers.CharField(
+        source="get_furnishing_display", read_only=True
+    )
+    parking_value = serializers.CharField(
+        source="get_furnishing_display", read_only=True
+    )
 
     class Meta:
         model = RentalInfo
         fields = (
             "id",
+            "available_for_value",
             "available_for",
             "expected_rent",
             "expected_deposit",
             "negotiable",
             "maintenance",
+            "maintenance_value",
+            "available_for_value",
             "available_from",
             "tenants",
+            "tenants_value",
             "furnishing",
+            "furnishing_value",
+            "parking_value",
             "parking",
             "description",
             "property_info",
@@ -119,13 +168,22 @@ class PropertyDiscussionSerializer(serializers.ModelSerializer):
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    paint_value = serializers.CharField(source="get_paint_display",read_only=True)
+    cleaned_value = serializers.CharField(source="get_cleaned_display",read_only=True)
+    available_days_value = serializers.CharField(source="get_available_days_display",read_only=True)
+
+
+
     class Meta:
         model = Schedule
         fields = (
             "id",
             "paint",
+            "paint_value",
             "cleaned",
+            "cleaned_value",
             "available_days",
+            "available_days_value",
             "start_time",
             "end_time",
             "property_type",
@@ -136,9 +194,21 @@ class PropertyDetailSerializer(serializers.ModelSerializer):
     """
     this view returns details of property
     """
-    #choicefield
+
+    # choicefield for property
     listing_type = serializers.CharField(source="get_listing_type_display")
-    
+    property_type = serializers.CharField(source="get_property_type_display")
+    property_adtype = serializers.CharField(source="get_property_adtype_display")
+    apartment_type = serializers.CharField(source="get_apartment_type_display")
+    bhk_type = serializers.CharField(source="get_bhk_type_display")
+    floor = serializers.CharField(source="get_floor_display")
+    total_floor = serializers.CharField(source="get_total_floor_display")
+    age = serializers.CharField(source="get_bhk_type_display")
+    facing = serializers.CharField(source="get_bhk_type_display")
+    bhk_type = serializers.CharField(source="get_bhk_type_display")
+    membership_plan = serializers.CharField(source="get_membership_plan_display")
+    condition_type = serializers.CharField(source="get_bhk_type_display")
+
     locations = LocationSerializer(read_only=True)
     rental_info = RentalSerializer(read_only=True, many=True)
     gallery = GallerySerializer(read_only=True, many=True)
@@ -169,6 +239,7 @@ class PropertyDetailSerializer(serializers.ModelSerializer):
             "gallery",
             "amenities",
             "listing_type",
-            
+            "condition_type",
+            "membership_plan",
         )
 
