f96be7e1dbd8c9a3083757b371a1510b9654b2d1
MadanStha7 <madanstha.124@gmail.com>
2021-05-04 15:36:57 +0545

    new commit

--- api/urls/property.py
+++ api/urls/property.py
@@ -13,20 +13,13 @@ from api.viewsets.property_viewset import (
     ScheduleViewSet,
     LocationViewSet,
     PropertyDetailsView,
-    PropertyViewSet,
-    FieldVisitViewSet,
-    PropertyDiscussionViewSet,
-    RentalViewSet,
-    GalleryViewSet,
-    AmentitesViewSet,
-    ScheduleViewSet,
     PropertyList,
     PropertyTop,
     PropertyPremium,
     PropertyFeatured,    
     PropertySearchView,
     CityListView,
-    DetailPropertyView
+    PropertyFilterView
 )
 
 
@@ -39,11 +32,12 @@ router.register(r"rental", RentalViewSet)
 router.register(r"gallery", GalleryViewSet)
 router.register(r"amenities", AmentitesViewSet)
 router.register(r"schedule", ScheduleViewSet)
+router.register(r"property-filter", PropertyFilterView)
 
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("property-list/", PropertyList.as_view(), name="list_property"),
+    path("property-list/", PropertyList.as_view(), name="list_property"), 
     path("property-detail/<int:pk>/", PropertyDetailsView.as_view(), name="property_detail"),
 
     path("property-category/top/", PropertyTop.as_view(), name="property_top"),
@@ -53,7 +47,6 @@ urlpatterns = [
     path(
         "property-category/featured/", PropertyFeatured.as_view(), name="property_featured",
     ),
-    path('detail-property/', DetailPropertyView.as_view(), name="detail-property"),
     # search
     path('locations/', PropertySearchView.as_view(), name="search_property"),
     path('city/', CityListView.as_view(), name="city"),
