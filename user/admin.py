b1341ee3b6cc660596b1075eb514850a38a38d29
viper thapa <thaparam78666@gmial.com>
2021-05-13 15:01:08 +0545

    added adminprofile in admin

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from user.models import AgentDetail, UserProfile, StaffDetail, Contact
+from user.models import AgentDetail, UserProfile, StaffDetail, Contact, AdminProfile
 
 # Register your models here.
-admin.site.register(
-    [
-        AgentDetail,
-        UserProfile,
-        StaffDetail,
-        Contact,
-    ]
-)
+admin.site.register([AgentDetail, UserProfile, StaffDetail, AdminProfile, Contact])
