52cc6d947805f560e209e1830c28645e907f52e7
viperthapa <thaparam78666@gmail.com>
2021-06-06 15:23:12 +0545

    customization of auth token

--- project/urls.py
+++ project/urls.py
@@ -17,12 +17,13 @@ from rest_framework.authtoken import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static, settings
+from .custom_token import CustomAuthToken
 
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/v1/", include("api.urls.core")),
-    path("api/v1/api-token-auth/", views.obtain_auth_token),
+    path("api/v1/api-token-auth/", CustomAuthToken.as_view()),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
