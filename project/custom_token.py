bbe9644e7747b92f848210267ae553f454577e40
viperthapa <thaparam78666@gmail.com>
2021-06-09 10:32:53 +0545

    group name and image added for logged in user

--- project/custom_token.py
+++ project/custom_token.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from user.models import AgentDetail, UserProfile, AdminProfile, StaffDetail
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 User = get_user_model()
 
@@ -18,33 +19,45 @@ class CustomAuthToken(ObtainAuthToken):
         try:
             user_details = AgentDetail.objects.get(user=user_obj)
             full_name = user_details.full_name
+            image = None
         except AgentDetail.DoesNotExist:
             pass
         try:
             user_details = StaffDetail.objects.get(user=user_obj)
             full_name = user_details.full_name
+            image = None
+
         except StaffDetail.DoesNotExist:
             pass
 
         try:
             user_details = UserProfile.objects.get(user=user_obj)
             full_name = user_details.full_name
-
+            if user_details.profile_picture:
+                image = user_details.profile_picture.url
+            else:
+                image = None
         except UserProfile.DoesNotExist:
             pass
         try:
             user_details = AdminProfile.objects.get(user=user_obj)
             full_name = user_details.full_name
-
+            if user_details.image:
+                image = user_details.image.url
+            else:
+                image = None
         except AdminProfile.DoesNotExist:
             pass
 
         token, created = Token.objects.get_or_create(user=user)
+        group_name = Group.objects.filter(user=user).values()
         return Response(
             {
                 "token": token.key,
                 "user_id": user.pk,
                 "email": user.email,
                 "full_name": full_name,
+                "image": image,
+                "group_name": group_name,
             }
         )
