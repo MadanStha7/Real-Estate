6cba66e86fb157eda192703708372da3c0366aa1
viperthapa <thaparam78666@gmail.com>
2021-07-11 13:03:08 +0545

    assign property request serialzer

--- api/urls/property.py
+++ api/urls/property.py
@@ -22,6 +22,7 @@ from api.viewsets.property_viewset import (
     PropertyRequestViewSet,
     PropertyDiscussionViewSet,
     ListedPropertyViewSet,
+    AssignPropertyRequestViewset,
 )
 
 router = routers.DefaultRouter()
@@ -51,5 +52,6 @@ router.register(r"property-request", PropertyRequestViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("assign-property/<int:pk>/", AssignPropertyViewset.as_view()),
+    path("assign-property-request/<int:pk>/", AssignPropertyRequestViewset.as_view()),
     path("admin_dashboard/", DashBoardView.as_view(), name="admin_dashboard"),
 ]
